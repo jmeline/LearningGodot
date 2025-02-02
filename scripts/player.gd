7bee4f40892e449556f061960e03d7c4e913888f
Jacob Meline <6642964+jmeline@users.noreply.github.com>
2025-02-02 16:41:31 -0700

    Add joystick support

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,14 +11,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("a_button") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	var input_direction = Input.get_axis("left", "right")
+	#var input_direction = Input.get_vector("left", "right", "up", "down")
+	#velocity = input_direction * SPEED
+	
+	if input_direction:
+		velocity.x = input_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
